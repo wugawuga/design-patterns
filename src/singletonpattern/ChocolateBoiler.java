d8e7ce141098578ac12381fa27086b6785229841
wugawuga <dnr3655@naver.com>
2022-10-24 13:38:07 +0900

    feat: 초콜릿 보일러

--- src/singletonpattern/ChocolateBoiler.java
+++ src/singletonpattern/ChocolateBoiler.java
@@ -1,2 +1,34 @@
-package singletonpattern;public class ChocolateBoiler {
+package singletonpattern;
+
+public class ChocolateBoiler {
+
+	private boolean empty;
+	private boolean boiled;
+
+	private ChocolateBoiler() {
+		this.empty = true;
+		this.boiled = false;
+	}
+
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+		}
+	}
+
+	public void boil() {
+		if (!isEmpty() && !isBoiled()) {
+			// 재료를 끓임
+			boiled = true;
+		}
+	}
+
+	private boolean isBoiled() {
+		return boiled;
+	}
+
+	private boolean isEmpty() {
+		return empty;
+	}
 }
