e86f9b46020d73cca3e9f72e4e68d7eadfc01239
wugawuga <dnr3655@naver.com>
2022-12-02 18:32:23 +0900

    feat: Exception template 추가

---
+++ src/templatemethodpattern/ExceptionTemplate.java
@@ -0,0 +1,16 @@
+package templatemethodpattern;
+
+public interface ExceptionTemplate {
+
+    default Object check() {
+        while (true) {
+            try {
+                return input();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    Object input() throws IllegalArgumentException;
+}
